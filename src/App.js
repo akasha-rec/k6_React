2fbdbeda37aab3c178dde5144ed87828ed08e0de
akasha-rec <uspira@naver.com>
2024-03-08 17:19:33 +0900

    react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          K-digital 6기 이나영
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
