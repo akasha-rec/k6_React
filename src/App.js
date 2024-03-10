51be62dcc4559b1017365ac7d51c11b6cf278258
akasha-rec <uspira@naver.com>
2024-03-11 08:56:16 +0900

    연결되나요?

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// svg(image)도 모듈처럼 갖고 와서 사용
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           K-digital 6기 이나영
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// css도 module처럼 import
 import './index.css';
+// App.js > module이자 component. 사용할 때는 tag처럼 = 사용자 정의 태그
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root에다가 App을 갖다 끼운다. component = function > return 되는 게 박힌다. > html tag
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
