361448463fb54afdd21ef91c60832b3bc0b20161
akasha-rec <uspira@naver.com>
2024-03-12 12:24:00 +0900

    JSX 문법. CSS 적용방법

--- src/01/Hello.js
+++ src/01/Hello.js
@@ -1,9 +1,36 @@
-function Hello() {
+import "./Hello.css";
 
+function Hello() {
+    let n = Math.floor(Math.random() * 99) + 1;
+    
+    const st1 = {
+        backgroundColor: "darkblue",
+        color: "white"
+    }
+
+    // let s = '';
+    // if (n % 2== 0) s = "짝수";
+    // else s = "홀수";
+    let x;
     return (
         <h1>
-        Hello
-        </h1>
+        <span style={st1}>Hello</span>
+        <span style={{display:"inline-flex", margin:"10px"}}>
+        {n < 10 ? `0${n}` : n}
+        {/* 
+        {n < 10 && "0"}
+        {n} 
+        */}
+        </span>
+        {/* {n % 2 === 0 ? "짝수" : "홀수"} */}
+        {n % 2 === 0 
+        ? <span style={{color:"red"}}>짝수</span>
+        : <span style={{color:"blue"}}>홀수</span>
+        }
+        {n % 2 === 0 && "💕" } 
+        {n % 2 === 1 && "❤" }
+        {/* <p>{ x || "x는 undefined입니다."}</p> */}
+        </h1> 
     ); 
 }
 
