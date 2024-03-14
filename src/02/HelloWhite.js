14dedfa918ad5f7268375b03663ed0092c78337e
akasha-rec <uspira@naver.com>
2024-03-14 11:10:54 +0900

    원하는 곳에만 css 적용

--- src/02/HelloWhite.js
+++ src/02/HelloWhite.js
@@ -1,7 +1,8 @@
+import styles from './HelloWhite.module.css'
 export default function HelloWhite() {
   return (
     <div>
-      <h1 className='ch1'>안녕하세요</h1>
+      <h1 className={styles.ch1}>안녕하세요</h1>
     </div>
   )
 }
--- src/02/HelloYellow.js
+++ src/02/HelloYellow.js
@@ -1,7 +1,9 @@
+import styles from './HelloYellow.module.css'
+
 export default function HelloYellow() {
   return (
     <div>
-      <h1 className='ch1'>안녕하세요</h1>
+      <h1 className={styles.ch1}>안녕하세요</h1>
     </div>
   )
 }
