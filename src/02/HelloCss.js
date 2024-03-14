9191e5e3d7920132cda0f710a6476f110cc9e01f
akasha-rec <uspira@naver.com>
2024-03-14 10:52:57 +0900

    component 분리?

--- src/02/HelloCss.js
+++ src/02/HelloCss.js
@@ -1,11 +1,14 @@
 import logo from '../logo.svg';
 import './Hellostyle.css';
+import HelloWhite from './HelloWhite';
+import HelloYellow from './HelloYellow';
 
 export default function HelloCss() {
   return (
     <div className='main'>
         <img src={logo} className="imgLogo" alt="logo" />
-        <h1 className='ch1'>안녕하세요</h1>
+        <HelloWhite />
+        <HelloYellow />
     </div>
   );
 }
