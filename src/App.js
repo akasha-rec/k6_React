7b8dc42c714787f8fa4ba1a9c69fc4c6db698cef
akasha-rec <uspira@naver.com>
2024-03-14 10:23:40 +0900

    3/12 복습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 // svg(image)도 모듈처럼 갖고 와서 사용
 // import MainHeader from './01/MainHeader';
 // import Hello from './01/Hello';
+// import MyClock from './01_1/MyClock';
 import './App.css';
-import MyClock from './01_1/MyClock';
+import HelloCss from './02/HelloCss';
 
 function App() {
   return (
     // JSX 문법 사용
     <>
     <div className="App">
-    <MyClock />
+    {/* <MyClock /> */}
+    <HelloCss />
     </div>
     </>
   );
 }
-
-//화살표 함수로 가능
-//const App = () => {
-
-// return (
-// );
-// });
 
 //반드시 있어야 자바스크립트의 모듈형 함수 > index.js에서 사용하려면 / improt할 때 {} 안 써도 된다
 export default App;
