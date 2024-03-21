20205b81fff9c4d8abff1b56ed554a6ed886f0d3
akasha-rec <uspira@naver.com>
2024-03-21 12:27:57 +0900

    복습 끝...

--- src/07/FoodCard.js
+++ src/07/FoodCard.js
@@ -14,9 +14,9 @@ export default function FoodCard({ fObj }) {
         setIsClick(!isClick);
     }
     return (
-        <div className="w-11/12 flex justify-center items-center
-                    border"
-            onClick={handleIsClick}>
+        <div className="w-11/12 
+                        flex justify-center items-center border"
+                        onClick={handleIsClick}>
             <div className="w-1/6 max-w-xl">
                 <img src={fimg} alt={bank} />
             </div>
@@ -24,7 +24,7 @@ export default function FoodCard({ fObj }) {
                 <h1 className="text-xl font-bold">{fObj.사업장명}</h1>
                 <h2 className="">{fObj.운영주체명}</h2>
                 <h3 className="">{fObj['사업장 소재지']}</h3>
-                <h4 className="w-full h-8 text-sm
+                <h4 className="w-full h-8 text-sm truncate
                      bg-slate-600 text-white mt-5 p-2">
                         {/* 클릭했을 때만 나타나게 하고 싶다 */}
                     {isClick && `Tel : ${fObj['연락처(대표번호)']}, Fax : ${fObj['팩스번호']}`}</h4>
--- src/07/FoodMain.js
+++ src/07/FoodMain.js
@@ -1,18 +1,48 @@
 import fData from "./fooddata.json"
 import FoodCard from "./FoodCard"
+import TailButton from "../UI/TailButton";
+import { useState } from "react";
 
 export default function FoodMain() {
     console.log(fData)
-  const cards = fData.map(item =>
-    <FoodCard fObj={item} key={item.사업장명}/>
-    );
+    let c1 = fData.map(item => item["운영주체 분류"]);
+    c1 = new Set(c1);
+    c1 = [...c1]
+    console.log(c1)
+
+    const [cards, setCards] = useState();
+//????
+    const handleList=(citem)=>{
+      console.log(citem)
+      
+      const tm = fData.filter(item => item["운영주체 분류"] === citem)
+                      .map(item =>
+                        <FoodCard fObj={item} key={item.사업장명}/>
+                        );
+
+      setCards(tm);
+      // const cards = fData.map(item =>
+      //   <FoodCard fObj={item} key={item.사업장명}/>
+      //   );
+    }
+
+    const c1Bts = c1.map(item =>
+      <TailButton caption = {item} color="blue"
+                  key={item} handleClick={()=>handleList(item)}/>
+      );
 
     return (
-    <div className="w-full grid 
-                    grid-col-1 md:grid-cols-2
-                    xl:grid-cols-3
-                    gap-2">
+        <div className="w-full h-full flex flex-col justify-start items-center">
+        <div className="w-full bg-blue-100 p-5">
+        {c1Bts}
+        </div>
+        <div className="w-full grid grid-col-1 md:grid-cols-2
+                        xl:grid-cols-3
+                        gap-2">
+
+      {/* 버튼을 누를 때마다 바뀔 내용 > useState */}
       {cards}
+    </div>
     </div>
   )
 }
